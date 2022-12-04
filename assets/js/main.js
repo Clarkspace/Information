2617675fb38552e8be4bd60de1bfeb25c8621df3
클라크공간 <119383312+Clarkspace@users.noreply.github.com>
2022-12-04 22:11:02 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
-$("#titleBar .swap").on('click',function(){  $("body").toggleClass("nav-visible") });
-$("#page-wrapper").on('click',function(){  $("body").removeClass("nav-visible") });
+$("#titleBar .swap").off().on('click',function(){  $("body").toggleClass("nav-visible") });
+$("#page-wrapper").off().on('click',function(){  $("body").removeClass("nav-visible") });
 $(window).resize(function(){ 
 if (window.innerWidth >= 980) { $("body").removeClass("nav-visible"); $("#navPanel").css('transition-duration','initial'); }
 else if (window.innerWidth < 980){ $("#navPanel").css('transition-duration','500ms'); }
 });
+
+
+
+
